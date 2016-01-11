8239220b984b8f932dda08476018e83d4d034fb1
Evgeny-Itzhak <jecka87@mail.ru>
2016-01-11 15:34:56 +0200

    first commit

---
+++ src/com/company/luxoft/FibonacciTask.java
@@ -0,0 +1,30 @@
+package com.company.luxoft;
+
+import java.util.Arrays;
+
+public class FibonacciTask {
+
+    // 0 1 1 2 3 5 8 13 21
+
+    public static Integer countFibonacci(int number){
+
+        if (number<1) return 0;
+
+        int[] arrayFibonacci = new int[number];
+
+        for(int i=0; i<arrayFibonacci.length; i++){
+            if (i<0) { arrayFibonacci[i] = 0; return 0; }
+            if (i==1) {arrayFibonacci[i] = 1;}
+            if(i>1) {arrayFibonacci[i] = arrayFibonacci[i-2]+arrayFibonacci[i-1];}
+        }
+
+        System.out.println(Arrays.toString(arrayFibonacci));
+
+        return arrayFibonacci[number-1];
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(countFibonacci(-5));
+    }
+}
---
+++ src/com/company/luxoft/VampireNumbersTask.java
@@ -0,0 +1,4 @@
+package com.company.luxoft;
+
+public class VampireNumbersTask {
+}
